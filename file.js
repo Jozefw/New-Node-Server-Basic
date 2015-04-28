985200c80c11d56678bda2fcdb31cdbf156fd86c
Jozef <jozefw@sbcglobal.net>
2015-04-28 16:44:26 -0700

    add fs module

---
+++ file.js
@@ -0,0 +1,12 @@
+// include http module
+var http = require('http'),
+
+// add mysql file  fs is for reading and writing files
+
+fs = require('fs');
+
+// create server
+
+http.createServer(function(request,response){
+
+});
