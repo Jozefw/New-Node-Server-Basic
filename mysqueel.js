dbd653b0700fee9bbb004c80ecb747aaa9236b4e
Jozef <jozefw@sbcglobal.net>
2015-04-29 13:17:17 -0700

    update log file

--- mysqueel.js
+++ mysqueel.js
@@ -4,5 +4,15 @@ var http = require('http'),
 
 	fs.readFile('log.txt', function(error,logData){
 		if (error) throw error;
-		var text = logData.toString();
+		var text = logData.toString(),
+			results = {},
+			lines =text.split('\n');
+
+			lines.forEach(function(line){
+				var parts = line.split(' '),
+					letter = parts[1],
+					count = parseInt(parts[2]);
+
+			})
+
 	});
