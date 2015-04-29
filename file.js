f39faa19e317306f98de0a8aaf6b0ce115991b3c
Jozef <jozefw@sbcglobal.net>
2015-04-28 17:00:34 -0700

    file handling

--- file.js
+++ file.js
@@ -9,4 +9,20 @@ fs = require('fs');
 
 http.createServer(function(request,response){
 
+// add listener
+response.on('end', function(){
+
 });
+// read the file
+fs.readFile("test.txt", 'utf-8', function(error,data){
+	// write headers
+	response.writeHead(200,{
+		'Content-Type': 'text/plain',
+	});
+	data = parseInt(data,10) + 1;
+// write incremented number to file
+fs.writeFile('test.txt', data);
+// end response
+response.end("This was refreshed " + data + " times!");
+});
+}).listen(8080);
